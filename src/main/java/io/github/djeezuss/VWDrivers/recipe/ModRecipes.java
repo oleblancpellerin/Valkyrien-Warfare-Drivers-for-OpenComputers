8aa9319a898f8133249519708b2e0f8f0bebda76
Djeezuss <olivierleb25@gmail.com>
2017-11-07 13:37:19 -0500

    Fixed a crash caused by the new Addon System of VW
    The crash was located in ModRecipes, for the etheriumCrystal.

--- src/main/java/io/github/djeezuss/VWDrivers/recipe/ModRecipes.java
+++ src/main/java/io/github/djeezuss/VWDrivers/recipe/ModRecipes.java
@@ -1,12 +1,12 @@
 package io.github.djeezuss.VWDrivers.recipe;
 
 
-import ValkyrienWarfareWorld.ValkyrienWarfareWorldMod;
 import io.github.djeezuss.VWDrivers.block.ModBlocks;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import valkyrienwarfare.addon.world.ValkyrienWarfareWorld;
 
 /**
  * Created by Djeezuss on 2017-06-22.
@@ -15,7 +15,7 @@ public class ModRecipes {
 	public static void init() {
 		GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.shipInterface), "ICI", "TOT", "ICI",
 			'I', Items.IRON_INGOT,
-			'C', ValkyrienWarfareWorldMod.instance.etheriumCrystal,
+			'C', ValkyrienWarfareWorld.INSTANCE.etheriumCrystal,
 			'T', li.cil.oc.api.Items.get("transistor").createItemStack(1),
 			'O', Blocks.field_190976_dk); //Observer block
 	}
